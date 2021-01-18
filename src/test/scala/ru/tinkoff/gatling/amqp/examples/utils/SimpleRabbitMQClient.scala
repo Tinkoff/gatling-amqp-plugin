9e7fee526663b8daf536053c65d74be2f6bb3625
Michael Krausse <michael.krausse@directbox.com>
2021-01-18 07:55:06 +0100

    review findings: connections and channels become global variables and .gitignore file extended
    - as requested writeChannel and readChannel become global variables
    - removed readConnection and writeConnection because only channels is used
    - added something to .gitignore file

--- src/test/scala/ru/tinkoff/gatling/amqp/examples/utils/SimpleRabbitMQClient.scala
+++ src/test/scala/ru/tinkoff/gatling/amqp/examples/utils/SimpleRabbitMQClient.scala
@@ -8,14 +8,19 @@ import java.nio.charset.StandardCharsets
   * Simple RabbitMQClient which consumes messages from one broker and write them to other broker.
   */
 object SimpleRabbitMQClient {
+  private val readQueue = "readQueue"
+  private val readPort = 5672
+  private val readChannel = getConnection(readPort).createChannel()
+
+  private val writeQueue = "writeQueue"
+  private val writePort = 5673
+  private val writeChannel = getConnection(writePort).createChannel()
 
   val deliverCallback: DeliverCallback = {
     (consumerTag: String, message: Delivery) => {
       println("Received a message")
-      val connection = getConnection(writePort)
-      val channel = connection.createChannel()
-      channel.queueDeclare(writeQueue, true, false, false, null)
-      channel.basicPublish("", writeQueue, message.getProperties, "Message processed".getBytes(StandardCharsets.UTF_8))
+      writeChannel.queueDeclare(writeQueue, true, false, false, null)
+      writeChannel.basicPublish("", writeQueue, message.getProperties, "Message processed".getBytes(StandardCharsets.UTF_8))
     }
   }
 
@@ -23,37 +28,17 @@ object SimpleRabbitMQClient {
   }
 
   def readAndWrite(): String = {
-    val connection = getConnection(readPort)
-    val channel = connection.createChannel()
-    channel.queueDeclare(readQueue, true, false, false, null)
-    channel.basicConsume(readQueue, true, deliverCallback, cancelCallback)
-
+    readChannel.queueDeclare(readQueue, true, false, false, null)
+    readChannel.basicConsume(readQueue, true, deliverCallback, cancelCallback)
   }
 
-  private val readQueue = "readQueue"
-  private val readPort = 5672
-
-  private val writeQueue = "writeQueue"
-  private val writePort = 5673
-
   def tearDown(): Unit = {
-    val readConnection: Connection = getConnection(readPort)
-    val readChannel = readConnection.createChannel()
     readChannel.queueDelete(readQueue)
-
-    val writeConnection: Connection = getConnection(writePort)
-    val writeChannel = writeConnection.createChannel()
     writeChannel.queueDelete(writeQueue)
-
   }
 
   def setup(): Unit = {
-    val readConnection: Connection = getConnection(readPort)
-    val readChannel = readConnection.createChannel()
     readChannel.queueDeclare(readQueue, true, false, false, null)
-
-    val writeConnection: Connection = getConnection(writePort)
-    val writeChannel = writeConnection.createChannel()
     writeChannel.queueDeclare(writeQueue, true, false, false, null)
   }
 
