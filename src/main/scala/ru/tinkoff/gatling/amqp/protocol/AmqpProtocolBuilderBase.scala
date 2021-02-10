0d61d3e7d2a8dc688d42880c769021893183099b
red-bashmak <v.kalyokin@tinkoff.ru>
2021-02-10 22:39:39 +0300

    feat: extends protocol dsl with init actions (declare, bind queues and exchanges)

--- src/main/scala/ru/tinkoff/gatling/amqp/protocol/AmqpProtocolBuilderBase.scala
+++ src/main/scala/ru/tinkoff/gatling/amqp/protocol/AmqpProtocolBuilderBase.scala
@@ -3,6 +3,8 @@ package ru.tinkoff.gatling.amqp.protocol
 import com.rabbitmq.client.ConnectionFactory
 
 case object AmqpProtocolBuilderBase {
-  def connectionFactory(cf: ConnectionFactory) = AmqpProtocolBuilder(cf, cf)
-  def connectionFactory(requestConnectionFactory: ConnectionFactory, replyConnectionFactory: ConnectionFactory) = AmqpProtocolBuilder(requestConnectionFactory, replyConnectionFactory)
+  def connectionFactory(cf: ConnectionFactory): AmqpProtocolBuilder = AmqpProtocolBuilder(cf, cf)
+  def connectionFactory(requestConnectionFactory: ConnectionFactory,
+                        replyConnectionFactory: ConnectionFactory): AmqpProtocolBuilder =
+    AmqpProtocolBuilder(requestConnectionFactory, replyConnectionFactory)
 }
