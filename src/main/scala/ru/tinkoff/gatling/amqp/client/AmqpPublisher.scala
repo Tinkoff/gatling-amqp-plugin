240bae49fb647e15c805f4b808bb59afa6f84046
red-bashmak <v.kalyokin@tinkoff.ru>
2021-02-10 22:44:10 +0300

    refactor: removed now unused declaration actions before sending data

--- src/main/scala/ru/tinkoff/gatling/amqp/client/AmqpPublisher.scala
+++ src/main/scala/ru/tinkoff/gatling/amqp/client/AmqpPublisher.scala
@@ -1,43 +1,30 @@
 package ru.tinkoff.gatling.amqp.client
 
 import io.gatling.core.session.Session
-import javax.jms.DeliveryMode
 import ru.tinkoff.gatling.amqp.action.Around
 import ru.tinkoff.gatling.amqp.protocol.AmqpComponents
-import ru.tinkoff.gatling.amqp.request.{AmqpDirectExchange, AmqpTopicExchange, AmqpExchange, AmqpProtocolMessage, AmqpQueueExchange}
+import ru.tinkoff.gatling.amqp.request._
 
-import scala.collection.JavaConverters._
 
 class AmqpPublisher(destination: AmqpExchange, components: AmqpComponents) extends WithAmqpChannel {
   def publish(message: AmqpProtocolMessage, around: Around, session: Session): Unit = {
 
-    val protocolDurable = components.protocol.deliveryMode == DeliveryMode.PERSISTENT
-
     destination match {
-      case AmqpDirectExchange(name, routingKey, durable) =>
+      case AmqpDirectExchange(name, routingKey, _) =>
         for {
           exName <- name(session)
           exKey  <- routingKey(session)
-        } withChannel { channel =>
-          channel.exchangeDeclare(exName, "direct", durable || protocolDurable)
-          around(channel.basicPublish(exName, exKey, message.amqpProperties, message.payload))
-        }
+        } withChannel(channel => around(channel.basicPublish(exName, exKey, message.amqpProperties, message.payload)))
 
-      case AmqpQueueExchange(name, durable) =>
+      case AmqpQueueExchange(name, _) =>
         name(session).foreach(qName =>
-          withChannel { channel =>
-            channel.queueDeclare(qName, durable || protocolDurable, false, false, Map.empty[String, Object].asJava)
-            around(channel.basicPublish("", qName, message.amqpProperties, message.payload))
-        })
+          withChannel(channel => around(channel.basicPublish("", qName, message.amqpProperties, message.payload))))
 
-      case AmqpTopicExchange(name, routingKey, durable) =>
+      case AmqpTopicExchange(name, routingKey, _) =>
         for {
           exName <- name(session)
           exKey  <- routingKey(session)
-        } withChannel { channel =>
-          channel.exchangeDeclare(exName, "topic", durable || protocolDurable)
-          around(channel.basicPublish(exName, exKey, message.amqpProperties, message.payload))
-        }
+        } withChannel(channel => around(channel.basicPublish(exName, exKey, message.amqpProperties, message.payload)))
     }
   }
 
