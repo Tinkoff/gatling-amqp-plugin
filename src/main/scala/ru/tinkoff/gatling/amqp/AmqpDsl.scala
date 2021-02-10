faceee6b754f49fb011203f6ad64582d62409e9c
red-bashmak <v.kalyokin@tinkoff.ru>
2021-02-10 22:40:42 +0300

    feat: aliases for declaration of queue and exchange

--- src/main/scala/ru/tinkoff/gatling/amqp/AmqpDsl.scala
+++ src/main/scala/ru/tinkoff/gatling/amqp/AmqpDsl.scala
@@ -1,6 +1,6 @@
 package ru.tinkoff.gatling.amqp
 
-import com.rabbitmq.client.ConnectionFactory
+import com.rabbitmq.client.{BuiltinExchangeType, ConnectionFactory}
 import io.gatling.core.action.builder.ActionBuilder
 import io.gatling.core.config.GatlingConfiguration
 import io.gatling.core.session.Expression
@@ -11,7 +11,7 @@ import ru.tinkoff.gatling.amqp.request.{AmqpDslBuilderBase, PublishDslBuilder, R
 trait AmqpDsl extends AmqpCheckSupport {
   def amqp(implicit configuration: GatlingConfiguration): AmqpProtocolBuilderBase.type = AmqpProtocolBuilderBase
 
-  def amqp(requestName: Expression[String]) = AmqpDslBuilderBase(requestName)
+  def amqp(requestName: Expression[String]): AmqpDslBuilderBase = AmqpDslBuilderBase(requestName)
 
   def rabbitmq(implicit configuration: GatlingConfiguration): RabbitMQConnectionFactoryBuilderBase.type =
     RabbitMQConnectionFactoryBuilderBase
@@ -19,6 +19,19 @@ trait AmqpDsl extends AmqpCheckSupport {
   def rabbitReceiver(implicit configuration: GatlingConfiguration): RabbitMQConnectionFactoryBuilderBase.type =
     RabbitMQConnectionFactoryBuilderBase
 
+  def queue(name: String,
+            durable: Boolean = true,
+            exclusive: Boolean = false,
+            autoDelete: Boolean = true,
+            arguments: Map[String, Any] = Map.empty): AmqpQueue =
+    AmqpQueue(name, durable, exclusive, autoDelete, arguments)
+
+  def exchange(name: String,
+               exchangeType: BuiltinExchangeType,
+               durable: Boolean = true,
+               autoDelete: Boolean = true,
+               arguments: Map[String, Any] = Map.empty): AmqpExchange =
+    AmqpExchange(name, exchangeType, durable, autoDelete, arguments)
 
   implicit def amqpProtocolBuilder2amqpProtocol(builder: AmqpProtocolBuilder): AmqpProtocol = builder.build
 
