afa43344cdfb2c738009e5a5027029fb22245eef
al.terekhov <al.terekhov@tinkoff.ru>
2023-02-10 10:47:11 +0300

    feat: add publish tests

--- src/test/scala/ru/tinkoff/gatling/amqp/examples/AmqpGatlingTest.scala
+++ src/test/scala/ru/tinkoff/gatling/amqp/examples/AmqpGatlingTest.scala
@@ -14,44 +14,65 @@ class AmqpGatlingTest extends Simulation {
   setUp(
     scenario("Test Scenario")
       .exec(
-        amqp("Test publish request").publish
+        amqp("Test publish queue exchange").publish
           .queueExchange("test_queue")
-          .textMessage("Publish test message")
+          .textMessage("Publish queue test message")
           .priority(0)
           .messageId("1")
-          .asScala(),
-      )
-      .exec(
-        amqp("Test queue exchange").requestReply
-          .queueExchange("test_queue")
-          .replyExchange("test_queue")
-          .textMessage("Queue test message")
-          .priority(0)
-          .messageId("1")
-          .expiration("60000")
+          .correlationId("1")
           .contentEncoding("text/plain")
+          .contentType("text")
           .asScala(),
       )
       .exec(
-        amqp("Test topic exchange").requestReply
-          .topicExchange("test_exchange", "routingKey")
-          .replyExchange("test_queue")
-          .textMessage("Topic test message")
+        amqp("Test publish topic exchange").publish
+          .topicExchange("test_exchange","routingKey")
+          .textMessage("Publish topic test message")
           .priority(0)
           .messageId("2")
           .asScala(),
       )
       .exec(
-        amqp("Test direct exchange").requestReply
+        amqp("Test publish direct exchange").publish
           .directExchange("test_exchange", "routingKey")
-          .replyExchange("test_queue")
-          .textMessage("Direct test message")
+          .textMessage("Publish direct test message")
           .priority(0)
           .messageId("3")
           .replyTo("test_queue")
           .appId("appId")
+          .asScala(),
+      )
+      .exec(
+        amqp("Test request reply queue exchange").requestReply
+          .queueExchange("test_queue")
+          .replyExchange("test_queue")
+          .textMessage("RR queue test message")
+          .priority(0)
+          .messageId("4")
+          .contentEncoding("text/plain")
+          .contentType("text")
+          .asScala(),
+      )
+      .exec(
+        amqp("Test request reply topic exchange").requestReply
+          .topicExchange("test_exchange", "routingKey")
+          .replyExchange("test_queue")
+          .textMessage("RR topic test message")
+          .priority(0)
+          .messageId("5")
+          .asScala(),
+      )
+      .exec(
+        amqp("Test request reply direct exchange").requestReply
+          .directExchange("test_exchange", "routingKey")
+          .replyExchange("test_queue")
+          .textMessage("RR direct test message")
+          .priority(0)
+          .messageId("6")
+          .replyTo("test_queue")
+          .appId("appId")
           .check(
-            simpleCheck(msg => msg.messageId.matches("3"))
+            simpleCheck(msg => msg.messageId.matches("6"))
           )
           .asScala(),
       )
@@ -73,6 +94,7 @@ class AmqpGatlingTest extends Simulation {
       .replyTimeout(60000)
       .consumerThreadsCount(8)
       .usePersistentDeliveryMode()
+      .matchByMessageId()
       .protocol(),
   ).maxDuration(20)
 }
