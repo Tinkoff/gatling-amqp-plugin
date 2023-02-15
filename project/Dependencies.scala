b5a226c7ef9e81a17b610547c7f2fb4d106b1967
al.terekhov <al.terekhov@tinkoff.ru>
2023-02-15 13:19:18 +0300

    fix: publishing

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -11,7 +11,7 @@ object Dependencies {
   lazy val gatling: Seq[ModuleID] = Seq(
     "io.gatling.highcharts" % "gatling-charts-highcharts",
     "io.gatling"            % "gatling-test-framework",
-  ).map(_ % gatlingVersion % Test)
+  ).map(_ % gatlingVersion)
 
   lazy val rabbitmq    = "com.rabbitmq"       % "amqp-client"   % "5.16.0"
   lazy val commonsPool = "org.apache.commons" % "commons-pool2" % "2.11.1"
